78961e7f77323457ea8a56df61bd4d984fa5e572
Maahi Batra <maahi.batra@gmail.com>
2025-03-31 19:49:39 +0530

    styled all pages and added 404

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res) => {
+    res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
+});
+
 app.listen(3000, () => {
     console.log('Server running on http://localhost:3000');
 });
